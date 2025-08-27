7afa03ecda73f8f6db3abd61eec165b1fc9c4a45
Kaveesha Chanduka <chanduka404@gmail.com>
2025-08-27 23:47:19 +0530

    persistant_storage scaffolding

---
+++ 6-persistent_storage/main.py
@@ -0,0 +1,39 @@
+import asyncio
+
+from dotenv import load_dotenv
+from google.adk.runners import Runner
+from google.adk.sessions import DatabaseSessionService
+
+
+
+
+
+load_dotenv
+
+# Part 1 - Intialize Persistant Session Service
+
+
+
+# Part 2 - Define inital state
+
+
+
+async def main_async():
+    # Setup constraints
+    APP_NAME="Memory agent"
+    USER_ID="Thanos"
+    
+    # Part 3 - Session Mangement.(Find if exisiting.If not create new session)
+
+
+
+    # Part 4 - Agent runner setup
+
+
+
+    # Part 5 - interactive conversation loop
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main_async)
